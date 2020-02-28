28111412ee93c4d6e24dbaf49c124714f66e42d2
wjhpower3 <61569239+wjhpower3@users.noreply.github.com>
2020-02-28 11:57:30 +0900

    Add files via upload

---
+++ audi_r8/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    function lamp(){
+        $(".skip").delay(1000).fadeIn(1000);
+        $(".top").delay(1500).animate({"width":"100%"}, 300, function(){
+            $(".top").animate({"left":"100%"}, 200, function(){
+                $(".top").css("width","0");
+            });
+        });
+        $(".bot").delay(1800).animate({"width":"100%"}, 300, function(){
+            $(".bot").animate({"left":"100%"}, 200, function(){
+                $(".bot").css("width","0");
+            });
+        });
+        $(".first").delay(3500).animate({"opacity":"1"},1000, function(){
+            $(".second").animate({"opacity":"1"},1000, function(){
+                $(".third").animate({"opacity":"1"},1000, function(){
+                    $(".first, .second, .third").delay(2000).animate({"opacity":"0"},1000, function(){
+                        $(".car .title").delay(1000).slideDown(500);
+                        $(".slide_menu").delay(1300).slideDown(500);
+                        $(".car img").animate({"left":"30%"},1000);
+                        $(".full").fadeOut(500);
+                    });
+                });
+            });
+        });
+    };
+    lamp();
+
+    $(".skip").click(function(){
+        $(".car .title").delay(500).slideDown(500);
+        $(".slide_menu").delay(500).slideDown(1000);
+        $(".car img").animate({"right":"-20px"},1000);
+        $(".full").fadeOut(500);
+    })
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+});
